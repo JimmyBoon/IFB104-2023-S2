ccedecaf3a1d3a6024d827802c7a23ef32219b3f
James Boon <jwboon@pm.me>
2023-08-11 10:43:09 +1000

    Start of week 3 workshop

---
+++ Week_3/challenge.py
@@ -0,0 +1,19 @@
+#Make this code more repeatable.
+
+animal1 = "Fox"
+animal2 = "Cow"
+animal3 = "Cat"
+animal4 = "Elephant"
+animal5 = "Lion"
+
+food1 = "Chickens"
+food2 = "Grass"
+food3 = "Fish"
+food4 = "Plants"
+food5 = "Animals"
+
+print("The " + animal1 + " eats " + food1)
+print("The " + animal2 + " eats " + food2)
+print("The " + animal3 + " eats " + food3)
+print("The " + animal4 + " eats " + food4)
+print("The " + animal5 + " eats " + food5)
