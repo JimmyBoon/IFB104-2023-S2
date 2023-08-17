0387e8fadd7451c71ed9051cb975db4417fe14a9
James Boon <jwboon@pm.me>
2023-08-18 09:49:03 +1000

    Start of week 4 workshop

---
+++ Week_4/challenge.py
@@ -0,0 +1,24 @@
+# Challenge:
+# Create a stupid automatic tic tac toe player.
+# If x_turn is true, then it is the x players go.
+# put an "X" in the next available spot.
+# Otherwise, it is the o players go.
+# put an "O" in the next available spot.
+# 
+# Use, loops, if/else, functions and modules.
+
+current_board = [["-","-","-"],["-","-","-"],["-","-","-"]]
+
+x_turn = True
+
+for row in current_board:
+    print(row)
+
+print("#################")
+
+if(current_board[0][0]) == "-" and x_turn:
+    current_board[0][0] = "X"
+
+
+for row in current_board:
+    print(row)
